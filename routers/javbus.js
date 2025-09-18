7d91407c697665e9f5c5f2b69d601a96c64bca63
ayyayyayy2002 <137018415+4evergr8@users.noreply.github.com>
2025-09-18 14:11:42 +0800

    Update javbus.js

--- routers/javbus.js
+++ routers/javbus.js
@@ -3,6 +3,7 @@ import {itemsToRss} from "../rss.js";
 
 export async function javbus(actorId,workerUrl) {
     const url = `https://www.javbus.com/star/${actorId}`
+    console.log("javbus:", actorId)
     const resp = await fetch(url, {
         headers: {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
